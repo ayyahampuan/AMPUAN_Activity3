5c2effb3c797e1fffde454b606d5d7c5f27e7ced
ayyahampuan <144468270+ayyahampuan@users.noreply.github.com>
2023-09-28 00:17:56 +0800

    Add files via upload

---
+++ AMPUAN.py
@@ -0,0 +1,5 @@
+seconds = eval(input("Enter the number of seconds: "))
+
+minutes = seconds // 60
+sec = seconds % 60
+print(seconds, "seconds is equal to" ,int(minutes), "minutes and",sec, "seconds.")
